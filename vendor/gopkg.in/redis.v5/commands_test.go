8c9a445d352b84b50996ca2611f4f4d0d8ba26c0
Takeshi Yamashita <yamacy.takecy@gmail.com>
2017-02-21 17:03:07 +0900

    update vendor

--- vendor/gopkg.in/redis.v5/commands_test.go
+++ vendor/gopkg.in/redis.v5/commands_test.go
@@ -50,6 +50,13 @@ var _ = Describe("Commands", func() {
 			Expect(ping.Val()).To(Equal("PONG"))
 		})
 
+		It("should Wait", func() {
+			// assume testing on single redis instance
+			wait := client.Wait(0, time.Minute)
+			Expect(wait.Err()).NotTo(HaveOccurred())
+			Expect(wait.Val()).To(Equal(int64(0)))
+		})
+
 		It("should Select", func() {
 			pipe := client.Pipeline()
 			sel := pipe.Select(1)
@@ -242,6 +249,14 @@ var _ = Describe("Commands", func() {
 			exists = client.Exists("key2")
 			Expect(exists.Err()).NotTo(HaveOccurred())
 			Expect(exists.Val()).To(Equal(false))
+
+			existsMul := client.ExistsMulti("key1", "key2")
+			Expect(existsMul.Err()).NotTo(HaveOccurred())
+			Expect(existsMul.Val()).To(Equal(int64(1)))
+
+			existsMul = client.ExistsMulti("key1", "key1")
+			Expect(existsMul.Err()).NotTo(HaveOccurred())
+			Expect(existsMul.Val()).To(Equal(int64(2)))
 		})
 
 		It("should Expire", func() {
@@ -1249,14 +1264,19 @@ var _ = Describe("Commands", func() {
 		})
 
 		It("should HVals", func() {
-			hSet := client.HSet("hash", "key1", "hello1")
-			Expect(hSet.Err()).NotTo(HaveOccurred())
-			hSet = client.HSet("hash", "key2", "hello2")
-			Expect(hSet.Err()).NotTo(HaveOccurred())
+			err := client.HSet("hash", "key1", "hello1").Err()
+			Expect(err).NotTo(HaveOccurred())
+			err = client.HSet("hash", "key2", "hello2").Err()
+			Expect(err).NotTo(HaveOccurred())
 
-			hVals := client.HVals("hash")
-			Expect(hVals.Err()).NotTo(HaveOccurred())
-			Expect(hVals.Val()).To(Equal([]string{"hello1", "hello2"}))
+			v, err := client.HVals("hash").Result()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(v).To(Equal([]string{"hello1", "hello2"}))
+
+			var slice []string
+			err = client.HVals("hash").ScanSlice(&slice)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(slice).To(Equal([]string{"hello1", "hello2"}))
 		})
 
 	})
@@ -2427,6 +2447,33 @@ var _ = Describe("Commands", func() {
 			val, err := client.ZRangeWithScores("zset", 0, -1).Result()
 			Expect(err).NotTo(HaveOccurred())
 			Expect(val).To(Equal([]redis.Z{{2, "two"}, {3, "three"}}))
+		})
+
+		It("should ZRemRangeByLex", func() {
+			zz := []redis.Z{
+				{0, "aaaa"},
+				{0, "b"},
+				{0, "c"},
+				{0, "d"},
+				{0, "e"},
+				{0, "foo"},
+				{0, "zap"},
+				{0, "zip"},
+				{0, "ALPHA"},
+				{0, "alpha"},
+			}
+			for _, z := range zz {
+				err := client.ZAdd("zset", z).Err()
+				Expect(err).NotTo(HaveOccurred())
+			}
+
+			n, err := client.ZRemRangeByLex("zset", "[alpha", "[omega").Result()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(n).To(Equal(int64(6)))
+
+			vals, err := client.ZRange("zset", 0, -1).Result()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(vals).To(Equal([]string{"ALPHA", "aaaa", "zap", "zip"}))
 		})
 
 		It("should ZRevRange", func() {
